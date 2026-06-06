90c5ff756e15067c8ab03e86f6234e3f39303897
fawakhry <87904105+fawakhry@users.noreply.github.com>
2026-06-06 09:10:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.MB_CONFIG = {
   businessName: "مطبعجي بنها",
   whatsappNumber: "201036112077", // غيّر الرقم لرقم مطبعجي بنها بصيغة دولية بدون +
-  activationEndpoint: "https://script.google.com/macros/s/AKfycbzexLEnuvpnxxlUediuy-oMdWIHR7EsXmeXr6EiyKLB0qN77EvDborrAJ-_LiHlLOoJ/exec",
+  activationEndpoint: "https://script.google.com/macros/s/AKfycbyEpE3Tx0j6g51C1svBGR7xjlF1EuZPVOpu5JjQ2aKXqFCAyzfzBNCCnA012Euc0_np/exec",
   sheetWidthCm: 29.7,
   sheetHeightCm: 45,
   dpi: 300,
